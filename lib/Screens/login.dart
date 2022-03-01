f5bc24799acaac519c81b8a29c5b81259790f0b9
icuriouswiz <73981868+cloneartist@users.noreply.github.com>
2022-03-01 12:57:08 +0530

    bug fix

--- lib/Screens/login.dart
+++ lib/Screens/login.dart
@@ -26,112 +26,109 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-            child: Form(
-              key: formKey,
-              child: Column(
-                children: [
-                  const Spacer(
-                    flex: 1,
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                const Spacer(
+                  flex: 1,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  "Welcome Back To\n       Daily Diary",
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                ),
+                const Spacer(flex: 1),
+                TextFormField(
+                  validator: (val) => val!.isEmpty ||
+                          !(val.contains('@') || !(val.contains('.com')))
+                      ? 'Enter a valid email address'
+                      : null,
+                  decoration: textInputDecoration.copyWith(
+                    labelText: 'Enter Your Email Address',
                   ),
-                  const SizedBox(
-                    height: 10,
+                  onChanged: (value) {
+                    email = value;
+                  },
+                ),
+                const SizedBox(height: 20.0),
+                TextFormField(
+                  obscureText: true,
+                  validator: (val) => val!.isEmpty || val.length < 6
+                      ? 'Enter a password greater than 6 characters'
+                      : null,
+                  decoration: textInputDecoration.copyWith(
+                    labelText: 'Enter Your Password',
                   ),
-                  const Text(
-                    "Welcome Back To\n       Daily Diary",
-                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  onChanged: (value) {
+                    password = value;
+                  },
+                ),
+                const SizedBox(height: 40.0),
+                ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.black,
+                    shape: const StadiumBorder(),
+                    padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
                   ),
-                  const Spacer(flex: 1),
-                  TextFormField(
-                    validator: (val) => val!.isEmpty || !(val.contains('@'))
-                        ? 'Enter a valid email address'
-                        : null,
-                    decoration: textInputDecoration.copyWith(
-                      labelText: 'Enter Your Email Address',
-                    ),
-                    onChanged: (value) {
-                      email = value;
-                    },
+                  icon: const Icon(
+                    Icons.login,
+                    color: Colors.white,
+                    size: 25,
                   ),
-                  const SizedBox(height: 20.0),
-                  TextFormField(
-                    obscureText: true,
-                    validator: (val) => val!.isEmpty || val.length < 6
-                        ? 'Enter a password greater than 6 characters'
-                        : null,
-                    decoration: textInputDecoration.copyWith(
-                      labelText: 'Enter Your Password',
-                    ),
-                    onChanged: (value) {
-                      password = value;
-                    },
-                  ),
-                  const SizedBox(height: 40.0),
-                  ElevatedButton.icon(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.black,
-                      shape: const StadiumBorder(),
-                      padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-                    ),
-                    icon: const Icon(
-                      Icons.login,
-                      color: Colors.white,
-                      size: 25,
-                    ),
-                    onPressed: () async {
-                      if (formKey.currentState!.validate()) {
-                        setState(() {});
-                        try {
-                          user = await auth.signInWithEmailAndPassword(
-                              email: email, password: password);
+                  onPressed: () async {
+                    if (formKey.currentState!.validate()) {
+                      setState(() {});
+                      try {
+                        user = await auth.signInWithEmailAndPassword(
+                            email: email, password: password);
 
-                          await signin(context);
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const NoteScreen()));
-                        } catch (e) {
-                          String error = e.toString();
-                          final loginerror = SnackBar(content: Text(error));
-                          ScaffoldMessenger.of(context)
-                              .showSnackBar(loginerror);
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const SignUp()));
-                        }
-                      }
-                    },
-                    label: const Text(
-                      "Login",
-                      style: TextStyle(color: Colors.white, fontSize: 15),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextButton(
-                      onPressed: () {
+                        await signin(context);
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const NoteScreen()));
+                      } on FirebaseAuthException catch (e) {
+                        String error = e.message.toString();
+                        final loginerror = SnackBar(content: Text(error));
+                        ScaffoldMessenger.of(context).showSnackBar(loginerror);
                         Navigator.push(
                             context,
                             MaterialPageRoute(
                                 builder: (context) => const SignUp()));
-                        setState(() {});
-                      },
-                      child: const Text(
-                        "New User? Sign Up",
-                        style: TextStyle(color: Colors.black),
-                      )),
-                  const Spacer(
-                    flex: 1,
-                  )
-                ],
-              ),
+                      }
+                    }
+                  },
+                  label: const Text(
+                    "Login",
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SignUp()));
+                      setState(() {});
+                    },
+                    child: const Text(
+                      "New User? Sign Up",
+                      style: TextStyle(color: Colors.black),
+                    )),
+                const Spacer(
+                  flex: 1,
+                )
+              ],
             ),
           ),
         ),
--- lib/Screens/signup.dart
+++ lib/Screens/signup.dart
@@ -25,122 +25,119 @@ class _SignUpState extends State<SignUp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-            child: Form(
-              key: formKey,
-              child: Column(
-                children: [
-                  const Spacer(
-                    flex: 1,
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                const Spacer(
+                  flex: 1,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  "Welcome To\n Daily Diary",
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                ),
+                const Spacer(flex: 1),
+                TextFormField(
+                  validator: (val) =>
+                      val!.isEmpty || val.isEmpty ? 'Enter your name' : null,
+                  decoration: textInputDecoration.copyWith(
+                    labelText: 'Enter Your Name',
                   ),
-                  const SizedBox(
-                    height: 10,
+                  onChanged: (value) {
+                    name = value;
+                  },
+                ),
+                const SizedBox(height: 20.0),
+                TextFormField(
+                  validator: (val) => val!.isEmpty ||
+                          !(val.contains('@') || !(val.contains('.com')))
+                      ? 'Enter a valid email address'
+                      : null,
+                  decoration: textInputDecoration.copyWith(
+                    labelText: 'Enter Your Email Address',
                   ),
-                  const Text(
-                    "Welcome To\n Daily Diary",
-                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  onChanged: (value) {
+                    email = value;
+                  },
+                ),
+                const SizedBox(height: 20.0),
+                TextFormField(
+                  obscureText: true,
+                  validator: (val) => val!.isEmpty || val.length < 6
+                      ? 'Enter a password greater than 6 characters'
+                      : null,
+                  decoration: textInputDecoration.copyWith(
+                    labelText: 'Enter Your Password',
                   ),
-                  const Spacer(flex: 1),
-                  TextFormField(
-                    validator: (val) =>
-                        val!.isEmpty || val.isEmpty ? 'Enter your name' : null,
-                    decoration: textInputDecoration.copyWith(
-                      labelText: 'Enter Your Name',
-                    ),
-                    onChanged: (value) {
-                      name = value;
-                    },
+                  onChanged: (value) {
+                    password = value;
+                  },
+                ),
+                const SizedBox(height: 40.0),
+                ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.black,
+                    shape: const StadiumBorder(),
+                    padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
                   ),
-                  const SizedBox(height: 20.0),
-                  TextFormField(
-                    validator: (val) => val!.isEmpty || !(val.contains('@'))
-                        ? 'Enter a valid email address'
-                        : null,
-                    decoration: textInputDecoration.copyWith(
-                      labelText: 'Enter Your Email Address',
-                    ),
-                    onChanged: (value) {
-                      email = value;
-                    },
+                  icon: const Icon(
+                    Icons.login,
+                    color: Colors.white,
+                    size: 25,
                   ),
-                  const SizedBox(height: 20.0),
-                  TextFormField(
-                    obscureText: true,
-                    validator: (val) => val!.isEmpty || val.length < 6
-                        ? 'Enter a password greater than 6 characters'
-                        : null,
-                    decoration: textInputDecoration.copyWith(
-                      labelText: 'Enter Your Password',
-                    ),
-                    onChanged: (value) {
-                      password = value;
-                    },
-                  ),
-                  const SizedBox(height: 40.0),
-                  ElevatedButton.icon(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.black,
-                      shape: const StadiumBorder(),
-                      padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-                    ),
-                    icon: const Icon(
-                      Icons.login,
-                      color: Colors.white,
-                      size: 25,
-                    ),
-                    onPressed: () async {
-                      if (formKey.currentState!.validate()) {
-                        setState(() {});
-                        try {
-                          UserCredential res =
-                              await _auth.createUserWithEmailAndPassword(
-                                  email: email, password: password);
+                  onPressed: () async {
+                    if (formKey.currentState!.validate()) {
+                      setState(() {});
+                      try {
+                        UserCredential res =
+                            await _auth.createUserWithEmailAndPassword(
+                                email: email, password: password);
 
-                          User? user = res.user;
-                          user!.updateDisplayName(name);
+                        User? user = res.user;
+                        user!.updateDisplayName(name);
 
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const NoteScreen()));
-                        } catch (e) {
-                          String error = e.toString();
-
-                          final loginerror = SnackBar(content: Text(error));
-                          ScaffoldMessenger.of(context)
-                              .showSnackBar(loginerror);
-                        }
-                      }
-                    },
-                    label: const Text(
-                      "Sign Up",
-                      style: TextStyle(color: Colors.white, fontSize: 15),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextButton(
-                      onPressed: () {
                         Navigator.push(
                             context,
                             MaterialPageRoute(
-                                builder: (context) => const Login()));
-                      },
-                      child: const Text(
-                        "Existing User? Login",
-                        style: TextStyle(color: Colors.black),
-                      )),
-                  const Spacer(
-                    flex: 1,
-                  )
-                ],
-              ),
+                                builder: (context) => const NoteScreen()));
+                      } on FirebaseAuthException catch (e) {
+                        String error = e.message.toString();
+
+                        final loginerror = SnackBar(content: Text(error));
+                        ScaffoldMessenger.of(context).showSnackBar(loginerror);
+                      }
+                    }
+                  },
+                  label: const Text(
+                    "Sign Up",
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Login()));
+                    },
+                    child: const Text(
+                      "Existing User? Login",
+                      style: TextStyle(color: Colors.black),
+                    )),
+                const Spacer(
+                  flex: 1,
+                )
+              ],
             ),
           ),
         ),
